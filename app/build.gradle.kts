35e2428f7af71ec222f0b28fdc055687ab08a1c1
Agamy <agamy207@gamil.com>
2025-02-12 23:00:25 +0200

    create splash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,9 +62,9 @@ dependencies {
     implementation("com.airbnb.android:lottie-compose:$lottieVersion")
 
     //retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation( "com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
 }
